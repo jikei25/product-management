5b9dceda7a8420ecb2862737cd2aec2102bc9bab
Jikei <khangphamcs@gmail.com>
2024-10-23 11:28:59 +0700

    Get products

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,5 +1,16 @@
-module.exports.index = (req, res) => {
+const Product = require("../../models/product.model");
+
+module.exports.index = async (req, res) => {
+    const products = await Product.find({
+        status: "active",
+        deleted: false
+    });
+    const newProducts = products.map(item => {
+        item.newPrice = (item.price - item.price * item.discountPercentage / 100).toFixed(2);
+        return item;
+    });
     res.render("client/pages/product/index", {
-        title: "Danh sách sản phẩm"
+        title: "Danh sách sản phẩm",
+        products: newProducts
     });
 }
